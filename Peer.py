9311d749f3ea88b722cf41c3b7895927822bc41c
rafias <asrafi@wp.pl>
2019-09-01 14:52:23 +0200

    not full class Peer and average ping metod added

--- Peer.py
+++ Peer.py
@@ -1,23 +1,34 @@
 from Magnet import Magnet
 import subprocess
-
+"""
+    This class represents nodes of p2p network
+    Attributes:
+        Magnet:magnet   :  Class which represents all data about file required by peer to connect
+        
+    Constructor:
+        Magnet:magnet   : It is adding magnet file to peer structure
+    Methods:
+        get_avg_ping    : From ip_list returns best 3 pings to classify best nodes of network
+"""
 class Peer:
     def __init__(self, path_to_magnet):
         self.magnet = Magnet(path_to_magnet)
 
 
-    def get_avg_ping(self, ip):
+    def get_avg_ping(self, ip_list):
         # Metod which is used to create list of best pings with thier ip
         sorted_list_with_ping = []
-        for i in ip:
-            s = subprocess.run(["ping", "-c 4", i[0]], stdout=subprocess.PIPE)
-            data_from_ping = str(s.stdout)
-            sorted_list_with_ping.append((data_from_ping.split("/")[4],i))
-        sorted_list_with_ping.sort()
-        top_3_ping_with_ip = sorted_list_with_ping[0:4]
 
-        
-        return top_3_ping_with_ip
+        for ip in ip_list:
+            s = subprocess.run(["ping", "-c 4", ip[0]], stdout=subprocess.PIPE)
+            data_from_ping = str(s.stdout)
+            sorted_list_with_ping.append((data_from_ping.split("/")[4],ip))
+        sorted_list_with_ping.sort()
+        if len(ip_list) < 3:
+            return sorted_list_with_ping
+        else:
+            return sorted_list_with_ping[0:4]
+
 
 
 
